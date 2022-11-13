f6a00cbc318e59fdcb9de2d9d4d364e3df3dc6ce
Nicolás Olmos <yohannicolas2003@gmail.com>
2022-11-13 15:34:22 -0500

    Database code for importing

---
+++ database/f1.sql
@@ -0,0 +1,44 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 13, 2022 at 09:08 PM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 8.1.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `f1`
+--
+CREATE DATABASE IF NOT EXISTS `f1` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `f1`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `carreras`
+--
+
+CREATE TABLE `carreras` (
+  `prix` varchar(25) NOT NULL,
+  `fecha` varchar(50) NOT NULL,
+  `ganador` varchar(60) NOT NULL,
+  `equipo` varchar(100) NOT NULL,
+  `vueltas` int(11) NOT NULL,
+  `tiempo` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
